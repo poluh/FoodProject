324c91909d9003385092e123508bcc0bd9faa77b
Denispok <vfkmxbr23@yandex.ru>
2018-04-29 16:00:20 +0300

    FUCKING WORKING BLYAT SUKA SHO??

--- app/src/main/java/thepiedpiper/com/foodproject/FilterActivity.kt
+++ app/src/main/java/thepiedpiper/com/foodproject/FilterActivity.kt
@@ -2,11 +2,13 @@ package thepiedpiper.com.foodproject
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 import thepiedpiper.com.foodproject.logic.read.CSVOpener
 import thepiedpiper.com.foodproject.logic.read.country.Item
 
-class FilterActivity: AppCompatActivity() {
+class FilterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,7 +22,14 @@ class FilterActivity: AppCompatActivity() {
         val spinner_year = findViewById<Spinner>(R.id.filter_spinner_year)
         spinner_year.adapter = YearAdapter(this, Item.allYears())
 
-        findViewById<android.support.v7.widget.SwitchCompat>(R.id.filter_switch).setOnCheckedChangeListener {
-            buttonView, isChecked -> spinner_item.adapter = ItemsAdapter(this, if (isChecked) feed else food) }
+        findViewById<android.support.v7.widget.SwitchCompat>(R.id.filter_switch).setOnCheckedChangeListener { buttonView, isChecked ->
+            spinner_item.adapter = ItemsAdapter(this, if (isChecked) feed else food)
+        }
+
+        val accept_button = findViewById<Button>(R.id.accept_button).setOnClickListener {
+            MainActivity.item = spinner_item.selectedItem as Item
+            MainActivity.year = spinner_year.selectedView.findViewById<TextView>(R.id.filter_spinner_text).text.toString().toInt()
+            onBackPressed()
+        }
     }
 }
